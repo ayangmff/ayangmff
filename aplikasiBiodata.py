fb3a3d7d7a58222108e6e4c0a2ba82b9da89f6c2
ayang mf <114496518+ayangmff@users.noreply.github.com>
2022-09-29 11:20:09 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,16 @@
+#aplilasi biodata create ayang
+
+#memasukan nama
+print ("yuk lengkapi data diri anda ")
+
+#input data diri 
+nama = input("nama          : ")
+kelas = input ("kelas       : ")
+prodi = input ("program studi")
+
+print("apakah data di bawah ini sudah benar ?")
+
+#print / output data diri 
+print ("nama anda adalah: ", nama)
+print ("kelas anda adalah ", kelas)
+
