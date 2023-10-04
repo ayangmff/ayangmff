4bb001fbdb2bb339de2dd1b0d29f51ad8681040c
ayang mf <114496518+ayangmff@users.noreply.github.com>
2023-10-04 19:54:25 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,10 @@
+//Identitas 
+var firstName = "Ayang";
+var lastName = "Fauziah";
+var age = 22;
+var isMarried = false;
+
+console.log(`FIRST NAME : ${firstName}`);
+console.log(`LAST NAME : ${lastName}`);
+console.log(`AGE : ${age}`);
+console.log(`IS MARRIED : ${isMarried}`);
