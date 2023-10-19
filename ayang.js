82e4a4435439b2e249f3832d9e66bfa1b2b17b37
ayang mf <114496518+ayangmff@users.noreply.github.com>
2023-10-19 20:17:28 +0700

    Add files via upload

---
+++ ayang.js
@@ -0,0 +1,40 @@
+console.log("Nama : Ayang M")
+console.log("NIM : 202200402460")
+console.log("Kelas : TI22A")
+console.log("--------------------------------------------------------------------------------")
+
+//Variabel dengan tipe objek
+let restaurant = {
+    name : "Resto A",
+    city : "Sukabumi",
+    favoriteDrink : "Manggo Juice",
+    favoriteFood : "Sausage",
+    isVegan : false
+};
+
+//Variabel dengan tipe array
+let restaurants = [
+    {
+    name : "Resto A",
+    city : "Sukabumi",
+    favoriteDrink : "Apple Juice",
+    favoriteFood : "Sausage",
+    isVegan : false
+    },
+{
+    name : "Resto B",
+    city : "Bandung",
+    favoriteDrink : "Iced Tea",
+    favoriteFood : "Burger",
+    isVegan : true
+    },
+    {   
+    name : "Resto C",
+    city : "Jakarta",
+    favoriteDrink: "Orange Juice",
+    favoriteFood : "Pizza",
+    isVegan : true  
+    }
+];
+console.log(restaurant);
+console.log(restaurants);
